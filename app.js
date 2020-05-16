39c894bcceebe77a17b8c8dede25dbd886fba6e0
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-16 20:33:07 +0500

    completed appjs

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = reqiure('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const tourRoutes = require('./routes/tourRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+// GLOBAL MIDDLEWARES
+// set security
+app.use(helmet());
+
+// development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// limit requests from name api
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+// bodyparser for reading data from body to req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization agaist NoSql query injections
+app.use(mongoSanitize());
+
+// data sanitization against XSS
+app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// ROUTES
+app.use('/api/v1/tours', tourRoutes);
+app.use('/api/v1/users', userRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
