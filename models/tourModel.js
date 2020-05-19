58db8e6cad7856f5054c1610d0620d75592a2f8f
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-19 21:58:29 +0500

    working on tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,1 +1,30 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have less or equal then 40 characters'],
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult',
+    },
+  },
+});
