8a7292f76fee85d4e22f3e8729f8194823ef313e
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-19 22:39:39 +0500

    completed the tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -98,3 +98,19 @@ tourSchema.pre(/^find/, function (next) {
   this.start = Date.now();
   next();
 });
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+//Aggregation middlware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
