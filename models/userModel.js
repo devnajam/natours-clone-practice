cf1f912313a2b8e349f9bbef2bd0f9bee7d8fc1d
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-20 20:23:02 +0500

    completed the model

--- models/userModel.js
+++ models/userModel.js
@@ -15,4 +15,35 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     validate: [validator.email, 'Please provide a valid email'],
   },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
