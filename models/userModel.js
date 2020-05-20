af54a7e06e667edc13e378beb161386fb0f6fcbd
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-20 20:20:24 +0500

    working on user model

---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.email, 'Please provide a valid email'],
+  },
+});
