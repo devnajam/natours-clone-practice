661fdb882193332100ddf0d07bfc1e8d96fee5c5
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-21 20:18:01 +0500

    started working on error Controller

---
+++ controllers/errorController.js
@@ -0,0 +1,13 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please enter another value!`;
+  return new AppError(message, 400);
+};
