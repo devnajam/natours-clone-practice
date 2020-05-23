62c183a0db819bf15b2a7448ad609b4a050f8105
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-23 09:04:31 +0500

    filter object user controller

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
