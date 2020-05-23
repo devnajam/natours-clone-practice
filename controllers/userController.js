4473e5aeffc972bc7419b34dbd9908fb65901c08
Najam Iftikhar <najamiftikhar2020@gmail.com>
2020-05-23 09:09:53 +0500

    get all users and update me

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,3 +9,28 @@ const filterObj = (obj, ...allowedFields) => {
   });
   return newObj;
 };
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  //send response
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // create error if user updates password
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. please use /updateMyPassword',
+        400
+      )
+    );
+  }
+});
